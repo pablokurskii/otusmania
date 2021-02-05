c8ef86d1718e3ae0b145f7c0f74752b0dd94d937
kurskii@ <kurskii@>
2021-02-05 12:52:01 +0100

    hw03-test-framework attempt 2

--- hw03-test-framework/src/main/java/homework/Main.java
+++ hw03-test-framework/src/main/java/homework/Main.java
@@ -1,14 +1,10 @@
 package homework;
 
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        TestExecutionResultPrinter testExecutionResultPrinter = new TestExecutionResultPrinter();
-        List<TestExecutionResultDetails> testExecutionResultDetails = new TestRunner(ClassTest.class).run();
-        testExecutionResultPrinter.print(testExecutionResultDetails);
+        MyTestFramework.runAllTests();
     }
 
 }
